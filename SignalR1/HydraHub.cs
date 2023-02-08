ec28f15a20e8d518db808b2b98d2d919dc2197dd
Efraim <Efraim@OZ-50>
2023-02-08 12:11:20 +0200

    Add project files.

---
+++ SignalR1/HydraHub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR1
+{
+    public class HydraHub : Hub
+    {
+
+        Timer timer;
+        public HydraHub()
+        {
+
+            //timer = new Timer(
+            //                new TimerCallback(Dgom),
+            //                null,
+            //                2000,
+            //                2000);
+
+        }
+
+        // This method will be called on Start button click
+        public async void Dgom(object state)
+        {
+            var temp = 36;
+            await Clients.All.SendAsync(@"{temp:" + temp.ToString() + "}");
+        }
+
+      
+    }
+}
---
+++ SignalR1/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using SignalR1;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddSignalR();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseCors(x => x
+                 .AllowAnyMethod()
+                 .AllowAnyHeader()
+                 .SetIsOriginAllowed(origin => true) // allow any origin
+                 .AllowCredentials()); // allow credentials
+
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapHub<HydraHub>("/Hydra");
+app.Run();
